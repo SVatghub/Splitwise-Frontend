c29c5769538bbaa74f103986dbf83b209ee5f849
sahil <sahil0000002@gmail.com>
2024-08-29 16:35:45 +0530

    Added a feature where each expense has a status associated to about settlement.

--- src/Expenses/ExpensesView/ExpenseView/Expense.jsx
+++ src/Expenses/ExpensesView/ExpenseView/Expense.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Expense.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
@@ -6,10 +6,33 @@ import { useNavigate } from 'react-router-dom';
 import Lottie from 'lottie-react';
 import { LuMoreVertical } from "react-icons/lu";
 import deleteAnimation from '../../../assets/deleteAnimation.json';  
+import { EXPENSES_API,states } from '../../../Constants/ApiConstants';
+import axios from 'axios';
 
 export default function Expense({ expense, userId, handleDelete }) {
+    const [state, setState] = useState(''); 
     const navigate = useNavigate();
     const [isAnimationPlaying, setIsAnimationPlaying] = useState(false);
+
+    useEffect(() => {
+        const fetchState = async () => {
+            try {
+                const response = await axios.get(EXPENSES_API.GET_EXPENSE_STATUS(userId,expense.id));
+                const responseData = await response.data; 
+                console.log(responseData);
+                if (responseData.userNotSettledDTOList.length === 0) {
+                    setState(states.CompletedlySettled);
+                } else if (responseData.userNotSettledDTOList.length !== 0 && responseData.userSettledDTOList.length !== 0) {
+                    setState(states.PartiallySettled);
+                } else {
+                    setState(states.NoSettlementReceived); 
+                }
+            } catch (error) {
+                console.error('Error fetching expense status:', error);
+            }
+        };
+        fetchState();
+    }, []);
 
     const formatDate = (dateString) => {
         const options = { day: '2-digit', month: 'short', year: 'numeric' };
@@ -58,7 +81,14 @@ export default function Expense({ expense, userId, handleDelete }) {
                             {formatDate(expense.createdAt)}
                         </p>
                     </div>
-                    <p className="expense-amount">&#8377;{expense.amount.toFixed(2)}</p>
+                    <div>
+                        <p className="expense-amount">&#8377;{expense.amount.toFixed(2)}</p>  
+                        { state === states.CompletedlySettled ?  <p className='expense-status expense-status-completed'> {state}</p> : null}  
+                        { state === states.PartiallySettled ?  <p className='expense-status expense-status-partially'> {state}</p> : null}  
+                        { state === states.NoSettlementReceived ?  <p className='expense-status expense-status-no-payment-received'> {state}</p> : null}          
+                       
+                    </div>
+                  
                 </>
             )}
         </div>
